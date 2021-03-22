7aed84360976769b7a2fe4039524df4b33bf67c1
kchu01 <chukathy01@gmail.com>
2021-03-22 16:27:45 -0700

    Add compponents and edit app.js

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+export default function Navbar() {
+
+    return (
+        <div>
+            <h1>Hello from Navbar</h1>
+        </div>
+    )
+
+}
---
+++ src/components/Welcome.jsx
@@ -0,0 +1,9 @@
+export default function Welcome() {
+
+    return (
+        <div>
+            <h1>Hello from Welcome</h1>
+        </div>
+    )
+
+}
