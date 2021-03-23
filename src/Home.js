3d84004928780197577e86ae1d65727ab8ac2f4e
kchu01 <chukathy01@gmail.com>
2021-03-22 22:28:46 -0700

    Add initial work on home.js and navbar

---
+++ src/Home.js
@@ -0,0 +1,53 @@
+import { useState, useEffect } from 'react'
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
+import jwt_decode from 'jwt-decode'
+
+import Welcome from './components/Welcome'
+import Navbar from './components/Navbar'
+import Register from './components/Register'
+import Login from './components/Login'
+import Profile from './components/Profile'
+import './Home.css';
+
+function Home() {
+
+  const [currentUser, setCurrentUser] = useState(null)
+
+  useEffect(() => {
+    const token = localStorage.getItem('jwtToken')
+    if (token) {
+      const decoded = jwt_decode(token)
+      setCurrentUser(decoded)
+    } else {
+      setCurrentUser(null)
+    }
+  }, [])
+
+  return (
+    // <div className="App">
+    //   <header className="App-header">
+    //     <Welcome />
+    //     <Navbar />
+    //     <Navbar />
+    //     <Navbar />
+
+    //   </header>
+    // </div>
+    <Router>
+      <header className="App-header">
+        {/* include a handlelogout */}
+        <Navbar currentUser={currentUser} />
+      </header>
+
+      <div>
+        <Switch>
+          {/* welcome page should display */}
+          {/* <Route exact path='/' render={(props) => <Login {...props} />} /> */}
+        </Switch>
+      </div>
+    </Router>
+
+  )
+}
+
+export default Home;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,42 @@
-export default function Navbar() {
+import { Link } from 'react-router-dom'
+// NEED TO ADD PETS AND HOTELS LINKS
+
+export default function Navbar(props) {
+    const loggedIn = (
+        <>
+            <Link to='/'>
+                <span onClick={props.handleLogout}>log out</span>
+            </Link>
+
+            <Link to='/profile'>
+                profile
+            </Link>
+        </>
+    )
+
+    const loggedOut = (
+        <>
+            <Link to='/register'>
+                register
+            </Link>
+            <br></br>
+            {/* use css to fix this instead of break line */}
+            <Link to='/login'>
+                login
+            </Link>
+        </>
+
+    )
 
     return (
-        <div>
-            <h1>Hello from Navbar</h1>
-        </div>
+        <nav className="navbar">
+            {/* sends to home or user? */}
+            <Link to='/'>
+                <h2>pawtel</h2>
+            </Link>
+
+            {props.currentUser ? loggedIn : loggedOut}
+        </nav>
     )
 
 }
