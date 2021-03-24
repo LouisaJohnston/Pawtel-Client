941ae9e05affb4b28d1fe0e295cfc4d5cf20bbea
LouisaJohnston <louisa.latham.johnston@gmail.com>
2021-03-24 10:11:54 -0700

    Working hotel show page

--- src/components/hotels/HotelDetails.jsx
+++ src/components/hotels/HotelDetails.jsx
@@ -2,8 +2,9 @@
 
 export default function HotelDetails(props) {
     return (
-        <div key={props.state.hotel}>
-            <h2> wooo {props.state.hotel_name} </h2>
+        <div key={props.location.state.hotel}>
+            <h2> {props.location.state.hotel_name} </h2>
+            
         </div>
     )
 }
