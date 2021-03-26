9e4b77f7616936cc684aa9903b9fa8e74b52d6a0
LouisaJohnston <louisa.latham.johnston@gmail.com>
2021-03-25 21:38:31 -0700

    Work on put/delete

--- src/components/pets/PetDetails.jsx
+++ src/components/pets/PetDetails.jsx
@@ -1,6 +1,42 @@
-import { Link } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import { Link, Redirect } from 'react-router-dom'
+import axios from 'axios'
 
 export default function PetDetails(props) {
+    const [deletedPet, setDeletedPet] = useState(false)
+
+    const handleUpdate =(e) => {
+        e.preventDefault()
+        const requestBody = {
+            // pet_name: pet_name,
+            // breed: breed,
+            // age: age,
+            // weight: weight,
+            // special_needs: special_needs,
+            // medications: medications
+        }
+        try {
+            axios.update(`${process.env.REACT_APP_SERVER_URL}/api-v1/pets`, requestBody)
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    const handleDelete = async (e) => {
+        e.preventDefault()
+        try {
+            const token = localStorage.getItem('jwtToken')
+            const authHeaders = {
+                'Authorization': token
+              }
+            await axios.delete(`${process.env.REACT_APP_SERVER_URL}/api-v1/pets`, { headers: authHeaders })
+            setDeletedPet(true)
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    if (deletedPet) return <Redirect to='/pets' />
     return (
         <div key={props.location.state.pet}>
             <h2> {props.location.state.pet_name} </h2>
@@ -9,6 +45,15 @@ export default function PetDetails(props) {
             <h4> {props.location.state.weight} </h4>
             <h4> {props.location.state.special_needs} </h4>
             <h4> {props.location.state.medications} </h4>
+
+            <form onSubmit={props.location.state.handleUpdate}>
+                <input type="submit" value="Update Pet"></input>
+            </form>
+
+            <form onSubmit={handleDelete}>
+                <input type="submit" value="Delete Pet"></input>
+            </form>
+
             <Link to='/pets'>Back to your pets</Link>
         </div>
     )
